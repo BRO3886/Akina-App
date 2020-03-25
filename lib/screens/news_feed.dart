e0bd4d839120bb1c2597bba3dcf5931f9d6f9512
BRO3886 <siddverma1999@gmail.com>
2020-03-25 17:39:08 +0530

    added requests screen and news screen

--- lib/screens/news_feed.dart
+++ lib/screens/news_feed.dart
@@ -1,10 +1,60 @@
 import 'package:flutter/material.dart';
 
+import '../utils.dart';
+
 class NewsFeedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            elevation: 0,
+            expandedHeight: MediaQuery.of(context).size.height * 0.15,
+            snap: true,
+            floating: true,
+            backgroundColor: Theme.of(context).canvasColor,
+            flexibleSpace: FlexibleSpaceBar(
+              titlePadding: EdgeInsets.only(
+                  bottom: 16, left: MediaQuery.of(context).size.width * 0.1),
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Text('News'),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.335,
+                  ),
+                  Container(
+                    height: 30,
+                    width: 30,
+                    child: CircleAvatar(
+                      backgroundColor: mainColor,
+                      child: Icon(
+                        Icons.account_circle,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  for (int i = 0; i < 10; i++)
+                    ListTile(
+                      title: Text('hi'),
+                      subtitle: Text('hello'),
+                    )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/screens/requests_feed.dart
+++ lib/screens/requests_feed.dart
@@ -1,11 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:project_hestia/utils.dart';
 
 class RequestsFeedScreen extends StatelessWidget {
   static const routename = '/feed';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            elevation: 0,
+            expandedHeight: MediaQuery.of(context).size.height * 0.15,
+            snap: true,
+            floating: true,
+            backgroundColor: Theme.of(context).canvasColor,
+            flexibleSpace: FlexibleSpaceBar(
+              titlePadding: EdgeInsets.only(
+                  bottom: 16, left: MediaQuery.of(context).size.width * 0.1),
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Text('Requests'),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.25,
+                  ),
+                  Container(
+                    height: 30,
+                    width: 30,
+                    child: CircleAvatar(
+                      backgroundColor: mainColor,
+                      child: Icon(
+                        Icons.account_circle,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  for (int i = 0; i < 10; i++)
+                    ListTile(
+                      title: Text('hi'),
+                      subtitle: Text('hello'),
+                    )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
